031689b17d4373ff71bfae7bd5852d2b6e701487
BilalHussain <hellobilalhussain@gmail.com>
2024-07-25 18:16:58 +0500

    Add Catch All Routes Fallback

---
+++ app/parallel-routes/@archive/[[...filter]]/page.js
@@ -0,0 +1,55 @@
+import Link from "next/link";
+import NewsList from "@/components/news-list";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
+
+export default function filteredNewsPage({ params }) {
+  const filter = params.filter;
+
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  return (
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/parallel-routes/${selectedYear}/${link}`
+                : `/parallel-routes/${link}`;
+
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {news && news.length > 0 ? (
+        <NewsList news={news} />
+      ) : (
+        <p>No news found for the selected year.</p>
+      )}
+    </>
+  );
+}
--- app/parallel-routes/@latest/default.js
+++ app/parallel-routes/@latest/default.js
@@ -3,6 +3,7 @@ import { getLatestNews } from "@/lib/news";
 
 export default function LatestNewsPage() {
   const latestNews = getLatestNews();
+  
   return (
     <>
       <h2>Latest News</h2>
